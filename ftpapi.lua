b0cb086dfe1e27dd574ee23bbf1b6c0fd88cbb15
afonya2 <adamhir@freemail.hu>
2023-04-17 20:30:10 +0200

    added ftpapi

---
+++ ftpapi.lua
@@ -0,0 +1,59 @@
+local api = {}
+local sga69 = require("sga69")
+local parts = 100
+
+function api.transmit(target, modem, port, handle)
+    function sendData()
+        while true do
+            local part = handle.read(parts)
+            if not part then break end
+            modem.transmit(port, port, {
+                mode = "DATA",
+                file = target,
+                data = part
+            })
+        end
+        handle.seek("set", 1)
+        local checksum = sga69(handle.readAll())
+        modem.transmit(port, port, {
+            mode = "END",
+            file = target,
+            checksum = checksum
+        })
+        modem.open(port)
+        local event, side, channel, replyChannel, message, distance = os.pullEvent("modem_message")
+        if message.mode == "RESEND" then
+            sendData()
+        end
+        modem.close(port)
+    end
+    sendData()
+    handle.close()
+end
+
+function api.receive(modem, port)
+    modem.open(port)
+    while true do
+        local event, side, channel, replyChannel, message, distance = os.pullEvent("modem_message")
+        if message.mode == "DATA" then
+            local h = fs.open(message.file, "ab")
+            h.write(message.data)
+            h.close()
+        elseif message.mode == "END" then
+            local h = fs.open(message.file, "rb")
+            local checksum = sga69(h.readAll())
+            h.close()
+            if checksum == message.checksum then
+                modem.transmit(port, port, {
+                    mode = "DONE"
+                })
+                break
+            else
+                modem.transmit(port, port, {
+                    mode = "RESEND"
+                })
+            end
+        end
+    end
+    modem.close(port)
+end
