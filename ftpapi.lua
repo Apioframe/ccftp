9a7c0aeeeae9e9b1880c8ed52429226fc5f83207
afonya2 <adamhir@freemail.hu>
2023-04-18 18:34:53 +0200

    fixed large files, file overwrite, better verification

--- ftpapi.lua
+++ ftpapi.lua
@@ -1,26 +1,42 @@
 local api = {}
 local sga69 = require("sga69")
-local parts = 100
+local parts = 500
 
 function api.transmit(target, modem, port, handle)
     function sendData()
+        modem.transmit(port, port, {
+            mode = "BEGIN",
+            file = target
+        })
+        local prevHash = ""
+        local fend = handle.seek("end")
         handle.seek("set", 0)
+        local i = 1
         while true do
             local part = handle.read(parts)
             if not part then break end
+            local hash = sga69(part, 32, 16)
             modem.transmit(port, port, {
                 mode = "DATA",
-                file = target,
-                data = part
+                data = part,
+                hash = hash,
+                prevHash = prevHash
             })
+            prevHash = hash
+            if i % 50 == 0 then
+                local percent = (handle.seek("cur") / fend) * 100
+                print((math.floor(percent*100)/100).."% ("..handle.seek("cur").."/"..fend..")")
+                os.sleep(0.1)
+            end
+            i = i + 1
         end
-        handle.seek("set", 0)
-        local checksum = sga69(handle.readAll(), 32, 16)
+        os.sleep(0.1)
+        local percent = (handle.seek("cur") / fend) * 100
+        print((math.floor(percent*100)/100).."% ("..handle.seek("cur").."/"..fend..")")
         modem.transmit(port, port, {
             mode = "END",
-            file = target,
-            checksum = checksum
         })
+        print("Verifying...")
         modem.open(port)
         local event, side, channel, replyChannel, message, distance = os.pullEvent("modem_message")
         if message.mode == "RESEND" then
@@ -34,26 +50,51 @@ end
 
 function api.receive(modem, port)
     modem.open(port)
+    local hashs = {}
+    local fil = nil
     while true do
         local event, side, channel, replyChannel, message, distance = os.pullEvent("modem_message")
         if message.mode == "DATA" then
-            local h = fs.open(message.file, "ab")
+            local h = fs.open(fil, "ab")
             h.write(message.data)
+            table.insert(hashs, {
+                data=message.data,
+                globall=message.hash,
+                msgprevHash=message.prevHash,
+            })
             h.close()
+        elseif message.mode == "BEGIN" then
+            if fs.exists(message.file) then
+                if fil == nil then
+                    fil = message.file
+                end
+                fs.delete(message.file)
+            end
         elseif message.mode == "END" then
-            local h = fs.open(message.file, "rb")
-            local checksum = sga69(h.readAll(), 32, 16)
-            h.close()
-            if checksum == message.checksum then
+            local resend = false
+            local prevHash = ""
+            print("Verifying...")
+            for k,v in ipairs(hashs) do
+                local hash = sga69(v.data, 32, 16)
+                if (hash ~= v.globall or prevHash ~= v.msgprevHash) then
+                    resend = true
+                    break
+                end
+                prevHash = hash
+                if k % 100 == 0 then
+                    os.sleep(0.1)
+                end
+            end
+            if not resend then
                 modem.transmit(port, port, {
                     mode = "DONE"
                 })
                 break
             else
-                fs.delete(message.file)
                 modem.transmit(port, port, {
                     mode = "RESEND"
                 })
+                resend = false
             end
         end
     end
